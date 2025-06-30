f308b897608218ba0e5b428c9ff20fd451aac7d2
lsg <hl771@outlook.com>
2025-06-30 12:28:02 +0800

    删除app.bat的注释；若带有注释，则win11终端无法正常运行。

--- app.bat
+++ app.bat
@@ -1,11 +1,15 @@
 @echo off
-:: 设置代码页为 UTF-8
+
+
+
 chcp 65001 >nul
 
-:: 设置工作目录为当前脚本所在目录
+
+
 cd /d "%~dp0"
 
-:: Step 1: 生成路径信息文件 path_location.json，生成cmd_app 文件以启动 命令行菜单
+
+
 echo [ bat ] The path configuration file is being generated...
 if exist "env\ltai\python.exe" (
     env\ltai\python.exe path_location.py
@@ -14,7 +18,8 @@ if exist "env\ltai\python.exe" (
     exit /b 1
 )
 
-:: Step 2: 从 cmd_app 文件中读取要执行的命令
+
+
 set "file=cmd_app"
 if not exist "%file%" (
     echo [ bat ] Error: file %file% does not exist!
@@ -27,9 +32,11 @@ if "%cmd_line%"=="" (
     exit /b 1
 )
 
-:: Step 3: 执行命令
+
+
 echo [ bat ] The command being executed: %cmd line%
 %cmd_line%
+
 
 echo [ bat ] All operations have been completed.
 
